0c5c5cd321455fa6a442278aab11e903788446a6
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-02-23 15:46:40 -0500

    created api file

---
+++ src/api.js
@@ -0,0 +1,16 @@
+// Base URL
+const base_url = 'https://api.rawg.io/api'
+
+// Get Date
+const currentYear = new Date().getFullYear();
+const currentMonth = String(new Date().getMonth() + 1).padStart(2, '0');
+const currentDay = String(new Date().getDate()).padStart(2, '0');
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+console.log(lastYear)
+//Popular Games 
+const popular_games = `/games?dates=${lastYear},${currentDate}&ordering=-rating$page_size=10`
+
+export const popularGamesURL = () => `${base_url}${popular_games}`
