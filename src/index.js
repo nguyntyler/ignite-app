9957b7df320cd07839439832f66915f556777d73
Tyler Nguyen <nguyn.tyler@gmail.com>
2021-02-24 14:38:15 -0500

    started actions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 //redux setup
-import {createStore} from 'redux'
-import rootReducer from './reducers'
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// store only takes two parameters. To add more, we need to use a workaround
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,17 +1,38 @@
+import axios from "axios";
+
 const initState = {
-    popular :[],
-    newGames : [],
-    upcoming : []
-}
+	popular: [],
+	newGames: [],
+	upcoming: [],
+};
 // These states are what is shown in redux's states. If we have more, we just create new files similar to this and add it to index.js
 
-const gamesReducer = (state=initState, action) => {
-    switch(action.type){
-        case 'FETCH_GAMES': 
-            return {...state}
-        default:
-            return {...state}
-    }
-}
+const gamesReducer = (state = initState, action) => {
+	switch (action.type) {
+		case "FETCH_GAMES":
+			return { ...state };
+		// { ..state, userData }
+		default:
+			return { ...state };
+	}
+};
 
-export default gamesReducer
+// Actions are objects that describe what is going to happen. It uses the reducer to know what happens.
+// Action creator is a function that returns an action.
+/*
+const fetchGames = (userData) => {
+    return {
+        type: 'FETCH_GAMES',
+        payload: userData
+    };
+};
+
+Running fetchGames(); will return the action itself.
+
+Action creators are most commonly used. 
+
+Parameters in the 
+*/
+// Redux Thunk. Vanilla redux does not have asynchnous features to use with api. Thunk allows asychronous fetching.
+
+export default gamesReducer;
